5d271910bca5f73334c9126f182113416e4d862e
John Paul Ahme <jpahme@yahoo.com>
2019-10-28 08:57:41 -0400

    integration tests

---
+++ 09_Capstone/java/src/test/java/com/techelevator/JDBCCampgroundDAOIntegrationTest.java
@@ -0,0 +1,47 @@
+package com.techelevator;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+import com.techelevator.campground.model.jdbc.JDBCCampgroundDAO;
+
+public class JDBCCampgroundDAOIntegrationTest {
+	private static SingleConnectionDataSource dataSource;
+	private JDBCCampgroundDAO dao;
+
+	@BeforeClass
+	public static void setUpDataSource() throws Exception {
+		dataSource = new SingleConnectionDataSource();
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/campground");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("postgres1");
+		dataSource.setAutoCommit(false);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws SQLException {
+		dataSource.destroy();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
